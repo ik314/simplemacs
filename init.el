ae3f4998d5903778859119aed0d3d47ff64d2c8b
Ismael Khan <ismaelk314@protonmail.com>
2023-02-25 23:19:00 +1000

    Stripped away a lot of stuff to get it to work :)

--- init.el
+++ init.el
@@ -38,35 +38,37 @@
 
 ;; company: Auto complete anything, anywhere
 ;; ------------------------------------------------------------------------
-(org-babel-load-file (expand-file-name
-		      "~/.emacs.d/modules/company.org"
-		      user-emacs-directory))
 
 ;; yasnippet: Snippet and be done with it
 ;; ------------------------------------------------------------------------
-(org-babel-load-file (expand-file-name
-		      "~/.emacs.d/modules/yasnippet.org"
-		      user-emacs-directory))
+;;(org-babel-load-file (expand-file-name
+;;		      "~/.emacs.d/modules/yasnippet.org"
+;;		      user-emacs-directory))
 
 
 ;; flyspell: Get the computer proof reading
 ;; ------------------------------------------------------------------------
-(org-babel-load-file (expand-file-name
-		      "~/.emacs.d/modules/flyspell.org"
-		      user-emacs-directory))
+;;(org-babel-load-file (expand-file-name
+;;		      "~/.emacs.d/modules/flyspell.org"
+;;		      user-emacs-directory))
+;;
+;;;; flycheck: Get the computer proof reading
+;;;; ------------------------------------------------------------------------
+;;(org-babel-load-file (expand-file-name
+;;		      "~/.emacs.d/modules/flycheck.org"
+;;		      user-emacs-directory))
+;;
+;;
+;;;; activities/programming: Minor modes and tweaks
+;;;; ------------------------------------------------------------------------
+;;(org-babel-load-file (expand-file-name
+;;		      "~/.emacs.d/modules/activities/programming.org"
+;;		      user-emacs-directory))
 
-;; flycheck: Get the computer proof reading
-;; ------------------------------------------------------------------------
-(org-babel-load-file (expand-file-name
-		      "~/.emacs.d/modules/flycheck.org"
-		      user-emacs-directory))
-
-
-;; activities/programming: Minor modes and tweaks
-;; ------------------------------------------------------------------------
-(org-babel-load-file (expand-file-name
-		      "~/.emacs.d/modules/activities/programming.org"
-		      user-emacs-directory))
+;; load custom org-books.el file
+(load (expand-file-name
+       "~/.emacs.d/modules/activities/org-books.el"
+       user-emacs-directory))
 
 ;;; init.el ends here
 
